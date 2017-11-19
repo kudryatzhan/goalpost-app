4514116e642c0ff17a07e595a3e47adb19528166
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-11-18 20:24:15 -0700

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Kudryatzhan Arziyev on 11/18/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
